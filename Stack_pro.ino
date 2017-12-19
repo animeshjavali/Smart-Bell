21b89659fd91a5d0024d1d5217f6572c016e083a
Animesh Javali <33710255+animeshjavali@users.noreply.github.com>
2017-12-19 10:54:15 -0800

    Update Stack_pro.ino

--- Stack_pro.ino
+++ Stack_pro.ino
@@ -1,34 +1,34 @@
 
 #include <DS3231.h>
-DS3231  rtc(SDA, SCL);
+DS3231  rtc(SDA, SCL);// Parse data and clock input.
 Time  t;
 void setup() {
-  Serial.begin(115200);
+ Serial.begin(115200);//Set serial commn at 115200 baudrate.
  rtc.begin(); 
   
 }
 
 void loop() {
-    t = rtc.getTime();
-   Serial.print(t.hour,DEC);
+   t = rtc.getTime();// 't'contains time info.
+   Serial.print(t.hour,DEC);// print time.
    Serial.print(" : ");
-   Serial.print(t.min,DEC);
-    Serial.print(" : ");
-    Serial.print(t.sec,DEC);
-    Serial.print("\n");
+   Serial.print(t.min,DEC);  
+   Serial.print(" : ");
+   Serial.print(t.sec,DEC);
+   Serial.print("\n");
    delay(1000);
-   if(t.hour==16 && t.min==12 && t.sec==00 )
+   if(t.hour==10 && t.min==40 && t.sec==00 ) // Data Analytics for deciding time to notify lecturers.
    {
-    Serial.println("ATD9535939593;");
+    Serial.println("ATD9535939***;"); // Place call at the required time.
     delay(25000);
- Serial.println("ATH");
+ Serial.println("ATH");// Auto-Hang up the call.
  Serial.println("AT+CMGF=1");
   delay(5000);
    Serial.print("AT+CMGS=\"");
-  Serial.print("9535939593");
+  Serial.print("95359395***");
   Serial.println("\"");
   delay(2000);
-  Serial.print("Good evening mam. Your class for 7TH sem starts in 5 mins");
+  Serial.print("Good evening mam. Your class for 7TH sem starts in 5 mins");// The text message.
   Serial.write(0x1A);
   
     }
